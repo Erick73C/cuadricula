faca8680ee843ec15d16f03c96566e03f57fe6c1
Erick Omar Perez Gonzlaez <s22120250@alumnos.itsur.edu.mx>
2025-09-24 20:42:13 -0600

    Se termino la actividad, agregando dos columnas

--- app/src/main/java/com/erick/cuadricula/data/DataSource.kt
+++ app/src/main/java/com/erick/cuadricula/data/DataSource.kt
@@ -5,25 +5,34 @@ import android.os.ext.SdkExtensions
 import androidx.annotation.RequiresExtension
 import com.erick.cuadricula.R
 import com.erick.cuadricula.R.string.*
-
+import com.erick.cuadricula.model.Topic
 
 object DataSource {
-
     @RequiresExtension(extension = SdkExtensions.AD_SERVICES, version = 4)
     val topics = listOf(
-        Topic(architecture.toLong(), 58, R.drawable.architecture),
-        Topic(crafts.toLong(), 121, R.drawable.crafts),
-        Topic(business.toLong(), 78, R.drawable.business),
-        Topic(culinary.toLong(), 118, R.drawable.culinary),
-        Topic(design.toLong(), 423, R.drawable.design),
-        Topic(fashion.toLong(), 92, R.drawable.fashion),
-        Topic(film.toLong(), 165, R.drawable.film),
-        Topic(gaming.toLong(), 164, R.drawable.gaming),
-        Topic(drawing.toLong(), 326, R.drawable.drawing),
-        Topic(lifestyle.toLong(), 305, R.drawable.lifestyle),
-        Topic(music.toLong(), 212, R.drawable.music),
-        Topic(painting.toLong(), 172, R.drawable.painting),
-        Topic(photography.toLong(), 321, R.drawable.photography),
-        Topic(tech.toLong(), 118, R.drawable.tech)
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.automotive,  30, R.drawable.automotive),
+        Topic(R.string.biology, 90, R.drawable.biology),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+        Topic(R.string.business, 78, R.drawable.business),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.design),
+        Topic(R.string.ecology, 28, R.drawable.ecology),
+        Topic(R.string.engineering, 67, R.drawable.engineering),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.finance, 100, R.drawable.finance),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 37, R.drawable.gaming),
+        Topic(R.string.geology, 290, R.drawable.geology),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.history, 189, R.drawable.history),
+        Topic(R.string.journalism, 96, R.drawable.journalism),
+        Topic(R.string.law, 58, R.drawable.law),
+        Topic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Topic(R.string.music, 212, R.drawable.music),
+        Topic(R.string.painting, 172, R.drawable.painting),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.physics, 41, R.drawable.physics),
+        Topic(R.string.tech, 118, R.drawable.tech),
     )
 }
